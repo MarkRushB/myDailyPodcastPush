11ab03ae9f7fbb09710f5cfc5e375448bcaf25ad
Mark <57408875+MarkRushB@users.noreply.github.com>
2024-01-02 03:24:09 +0000

    batch push notification

--- dailyPush.py
+++ dailyPush.py
@@ -1,9 +1,13 @@
 import requests
 import json
 import os
+import time
 
-BARK_API = os.environ.get("BARK_API")
-PODCAST_HOT_EPISODES_API = os.environ.get("PODCAST_HOT_EPISODES")
+# BARK_API = os.environ.get("BARK_API")
+# PODCAST_HOT_EPISODES_API = os.environ.get("PODCAST_HOT_EPISODES")
+
+BARK_API = "https://api.day.app/hoT3H6LmihHPftm4MFGqFX"
+PODCAST_HOT_EPISODES_API = "https://xyzrank.com/assets/hot-episodes.24ef12b57741554e245e301ea9240f8f.json"
 
 def get_podcast_info(url):
     try:
@@ -18,7 +22,8 @@ def get_podcast_info(url):
 
 data = get_podcast_info(PODCAST_HOT_EPISODES_API)
 
-print(data["data"]["episodes"][0])
+
+top_10_episodes = data["data"]["episodes"][:6]
 
 
 def push_ios_notification(url, data):
@@ -30,25 +35,25 @@ def push_ios_notification(url, data):
         response = requests.post(url, data=json_data, headers=headers)
         
         if response.status_code == 200:
-            print("POST请求成功！")
+            print("post done!")
         else:
-            print(f"POST请求失败，状态码：{response.status_code}")
-            print("响应内容：", response.text)
+            print(f"post failed, code: {response.status_code}")
+            print("resp: ", response.text)
     
     except requests.RequestException as e:
-        print(f"请求错误: {e}")
+        print(f"err: {e}")
     except ValueError:
-        print("无法解析 JSON 数据")
+        print("cannot parse json data!")
 
-data = {
-    "body": "Test Bark Server",
-    "title": "Test Title",
-    "badge": 1,
-    "category": "myNotificationCategory",
-    "sound": "minuet.caf",
-    "icon": "https://day.app/assets/images/avatar.jpg",
-    "group": "test",
-    "url": "https://mritd.com"
-}
 
-push_ios_notification(BARK_API, data)
+for episode in top_10_episodes:
+    data = {
+        "body": episode["title"],
+        "title": episode["podcastName"],
+        "category": "myNotificationCategory",
+        "icon": episode["logoURL"],
+        "group": "podcast",
+        "url": "https://xyzrank.com/#/"
+    }
+    push_ios_notification(BARK_API, data)
+    time.sleep(0.2)
