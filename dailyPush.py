55882b8ae83b2614f878e2e086950eb6b4b550d4
Mark <57408875+MarkRushB@users.noreply.github.com>
2024-01-02 00:26:10 +0000

    update ios push and manage secrects

--- dailyPush.py
+++ dailyPush.py
@@ -1,19 +1,54 @@
 import requests
+import json
+import os
 
-def fetch_and_parse_json(url):
+BARK_API = os.environ.get("BARK_API")
+PODCAST_HOT_EPISODES_API = os.environ.get("PODCAST_HOT_EPISODES")
+
+def get_podcast_info(url):
     try:
         response = requests.get(url)
         response.raise_for_status()  
         data = response.json()
         return data
     except requests.RequestException as e:
+        print(f"request error: {e}")
+    except ValueError:
+        print("cannot parse json data!")
+
+data = get_podcast_info(PODCAST_HOT_EPISODES_API)
+
+print(data["data"]["episodes"][0])
+
+
+def push_ios_notification(url, data):
+    try:
+        json_data = json.dumps(data)
+        headers = {
+            "Content-Type": "application/json; charset=utf-8"
+        }
+        response = requests.post(url, data=json_data, headers=headers)
+        
+        if response.status_code == 200:
+            print("POST请求成功！")
+        else:
+            print(f"POST请求失败，状态码：{response.status_code}")
+            print("响应内容：", response.text)
+    
+    except requests.RequestException as e:
         print(f"请求错误: {e}")
     except ValueError:
         print("无法解析 JSON 数据")
 
+data = {
+    "body": "Test Bark Server",
+    "title": "Test Title",
+    "badge": 1,
+    "category": "myNotificationCategory",
+    "sound": "minuet.caf",
+    "icon": "https://day.app/assets/images/avatar.jpg",
+    "group": "test",
+    "url": "https://mritd.com"
+}
 
-url = "https://xyzrank.com/assets/hot-episodes.24ef12b57741554e245e301ea9240f8f.json"
-
-
-data = fetch_and_parse_json(url)
-print(data)
+push_ios_notification(BARK_API, data)
