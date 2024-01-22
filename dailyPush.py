eae110f792d912b69b1b35993f3b3c606b15bb03
Mark <57408875+MarkRushB@users.noreply.github.com>
2024-01-22 00:07:04 +0000

    udpate script

--- dailyPush.py
+++ dailyPush.py
@@ -1,27 +1,14 @@
 import requests
 import json
 import os
-import time
+import easyocr
+from datetime import datetime
+from PIL import Image
+from io import BytesIO
 
+
+reader = easyocr.Reader(['ch_sim', 'en'])
 BARK_API = os.environ.get("BARK_API")
-PODCAST_HOT_EPISODES_API = os.environ.get("PODCAST_HOT_EPISODES")
-
-
-def get_podcast_info(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  
-        data = response.json()
-        return data
-    except requests.RequestException as e:
-        print(f"request error: {e}")
-    except ValueError:
-        print("cannot parse json data!")
-
-data = get_podcast_info(PODCAST_HOT_EPISODES_API)
-
-
-top_10_episodes = data["data"]["episodes"][:6]
 
 
 def push_ios_notification(url, data):
@@ -43,15 +30,95 @@ def push_ios_notification(url, data):
     except ValueError:
         print("cannot parse json data!")
 
+def get_date_url():
+    # Get current date
+    d = datetime.now()
+    y = d.year
+    m = f"{d.month:02d}"  # Format month with leading zero if needed
+    n = f"{d.day:02d}"    # Format day with leading zero if needed
 
-for episode in top_10_episodes:
+    # Construct the URL
+    url = f"https://img.owspace.com/Public/uploads/Download/{y}/{m}{n}.jpg"
+    return url
+
+
+def crop_image(image_url):
+    response = requests.get(image_url)
+    if response.status_code == 200:
+
+        with open("image.jpg", "wb") as f:
+            f.write(response.content)
+
+        image = Image.open("image.jpg")
+
+        width, height = image.size
+
+        # The first section includes the day and the lunar date.
+        first_section = (0, 0, width, int(height * 0.15))
+
+        # The second section includes the phrase "宜"
+        second_section = (int(width * 0.2), int(height * 0.25), width - int(width * 0.2), int(height * 0.55))
+
+        # The third section includes the article excerpt and the author information.
+        third_section = (0, int(height * 0.55), width, height - int(height * 0.1))
+
+
+        # Crop the image into the three specified sections
+        first_img = image.crop(first_section)
+        second_img = image.crop(second_section)
+        third_img = image.crop(third_section)
+
+        first_img.save("top_part.jpg")
+        second_img.save("middle_part.jpg")
+        third_img.save("bottom_part.jpg")
+        
+        return first_img, second_img, third_img
+
+        print("image has been croped according to the requirement")
+    else:
+        print("Cannot download image")
+        return None, None, None
+
+def ocr(image):
+    try:
+        # Convert the image to bytes
+        image_bytes = BytesIO()
+        image.save(image_bytes, format="JPEG")
+        image_bytes = image_bytes.getvalue()
+
+        # Perform OCR on the image bytes
+        result = reader.readtext(image_bytes, detail=0)
+        print("OCR Result:", result)
+        return result
+    except Exception as e:
+        print("OCR error:", e)
+        return None
+
+
+def generate_data(date_list, recommendation_list, quote_list, url):
+    date_string = '\n'.join(date_list)
+    recommendation_string = next((item for item in recommendation_list if item.startswith('宜') or item.startswith('忌')), None)
+    quote_string = ' '.join(quote_list[:-1])
     data = {
-        "body": episode["title"],
-        "title": episode["podcastName"],
+        "body": quote_string,
+        "title": recommendation_string,
         "category": "myNotificationCategory",
-        "icon": episode["logoURL"],
-        "group": "podcast",
-        "url": "https://xyzrank.com/#/"
+        # "icon": episode["logoURL"],
+        "group": "calendar",
+        "url": url
     }
-    push_ios_notification(BARK_API, data)
-    time.sleep(0.2)
+    return data
+
+def do_it():
+    url = get_date_url()
+    first_section_img, second_section_img, third_section_img = crop_image(url)
+
+    if first_section_img:
+        date_list = ocr(first_section_img)
+    if second_section_img:
+        recommendation_list = ocr(second_section_img)
+    if third_section_img:
+        quote_list = ocr(third_section_img)
+    
+    push_payload = generate_data(date_list, recommendation_list, quote_list, url)
+    push_ios_notification(BARK_API, push_payload)
